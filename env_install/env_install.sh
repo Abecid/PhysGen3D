2f4f02d9d2062b567ff8df16ed64312ff05a775d
Adam Lee <cmlee3211@gmail.com>
2025-04-14 17:50:08 +0900

    Fix installation setup

--- env_install/env_install.sh
+++ env_install/env_install.sh
@@ -2,6 +2,9 @@ pip install -r requirements.txt
 pip install git+https://github.com/facebookresearch/pytorch3d.git
 
 pip install git+https://github.com/IDEA-Research/Grounded-SAM-2.git
+
+pip install -r requirements.txt --force-reinstall # reset torch to 2.1.0
+
 pip install git+https://github.com/IDEA-Research/GroundingDINO.git
 
 pip install ninja
